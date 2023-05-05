1da1d16d32c5b70e77d076d8623d2d2c0ca73770
shreecheryl <shreecheryl@att.net>
2023-05-05 16:03:09 -0700

    math exercises complete

--- js/math.js
+++ js/math.js
@@ -1,9 +1,38 @@
 //STEP 1
+// function positiveNum() {
+//     let num = parseFloat(prompt("Enter a number"));
+//     console.log(Math.abs(num));
+// }
+// positiveNum();
 
 //STEP 2
+// function roundUp() {
+//     let num = parseFloat(prompt("Enter a floating point value"));
+//     console.log(Math.ceil(num));
+// }
+// roundUp();
 
 //STEP 3
+// function roundDown() {
+//     let num = parseFloat(prompt("Enter a floating point value"));
+//     console.log(Math.floor(num));
+// }
+// roundDown();
 
 //STEP 4
+// function minMax() {
+//     let nums = prompt("Enter five comma delimited numbers");
+//     let arrNums = nums.split(",");
+//     arrNums = arrNums.map(number => parseInt(number));
+//     let minNum = Math.min(arrNums[0], arrNums[1], arrNums[2], arrNums[3], arrNums[4]);
+//     let maxNum = Math.max(arrNums[0], arrNums[1], arrNums[2], arrNums[3], arrNums[4]);
+//     console.log(`Mininum number: ${minNum} Maximum number: ${maxNum}`);
+// }
+// minMax();
 
 //STEP 5
+// function squareRoot() {
+//     let num = parseInt(prompt("Enter a number"));
+//     console.log(Math.sqrt(num));
+// }
+// squareRoot();
