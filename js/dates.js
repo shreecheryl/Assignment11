cc5d0632a640023602b4ae154143ade9d3ead961
shreecheryl <shreecheryl@att.net>
2023-05-06 10:30:21 -0700

    step 1 of dates complete

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,27 @@
 //STEP 1
-
+function daysInMonth() {
+    let months = [
+       'January',
+       'February',
+       'March',
+       'April',
+       'May',
+       'June',
+       'July',
+       'August',
+       'September',
+       'October',
+       'November',
+       'December'
+     ];
+   
+     let year = parseInt(prompt("Enter year"));    
+     let month = prompt("Enter month");
+     let monthNumber = months.indexOf(month) + 1;
+     let numberOfDays = new Date(year, monthNumber, 0).getDate(); 
+     console.log(`The number of days in ${month} in ${year} is ${numberOfDays}.`);
+   }
+ daysInMonth();
 //STEP 2
 
 //STEP 3
